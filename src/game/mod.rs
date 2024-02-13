ce5c1566c0a63e4c1ce5b5b369b5336a6ab5f9d2
loziuu <loz.iov@gmail.com>
2024-02-13 22:55:06 +0100

    Some opimisations tries

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,40 +1,26 @@
-const OFFSETS: [Offset; 8] = [
-    Offset::new(1, 0),
-    Offset::new(0, 1),
-    Offset::new(1, 1),
-    Offset::new(-1, 0),
-    Offset::new(0, -1),
-    Offset::new(-1, -1),
-    Offset::new(1, -1),
-    Offset::new(-1, 1),
+use std::time::Instant;
+
+const OFFSETS: [(i32, i32); 8] = [
+    (1, 0),
+    (0, 1),
+    (1, 1),
+    (-1, 0),
+    (0, -1),
+    (-1, -1),
+    (1, -1),
+    (-1, 1),
 ];
 
 pub const N_GRID: usize = 100;
 
-struct Offset {
-    x: i32,
-    y: i32,
-}
-
-impl Offset {
-    const fn new(x: i32, y: i32) -> Offset {
-        Offset { x, y }
-    }
-}
-
 pub struct Life {
     grid: [[bool; N_GRID]; N_GRID],
+    alive: Vec<(usize, usize)>,
 }
 
 pub struct Cords {
     x: usize,
     y: usize,
-}
-
-impl Cords {
-    pub fn new(x: usize, y: usize) -> Cords {
-        Cords { x, y }
-    }
 }
 
 impl Life {
@@ -43,7 +29,10 @@ impl Life {
         for cord in alive {
             grid[cord.x][cord.y] = true;
         }
-        Life { grid }
+        Life {
+            grid,
+            alive: Vec::new(),
+        }
     }
 
     pub fn current_generation(&self) -> &[[bool; N_GRID]; N_GRID] {
@@ -51,41 +40,112 @@ impl Life {
     }
 
     pub fn next_generation(&mut self) {
-        let old = self.grid.clone();
-        for x in 0..N_GRID {
-            for y in 0..N_GRID {
-                verify_cell(&old, &mut self.grid, x, y);
+        let mut visited = vec![vec![false; N_GRID]; N_GRID];
+
+        let mut x_cords: Vec<usize> = Vec::with_capacity(self.alive.len() * 9);
+        let mut y_cords: Vec<usize> = Vec::with_capacity(self.alive.len() * 9);
+
+
+        for cell in &self.alive {
+            x_cords.push(cell.0);
+            y_cords.push(cell.1);
+            for offset in OFFSETS {
+                let dx = sum(cell.0, offset.0);
+                let dy = sum(cell.1, offset.1);
+                if dx >= 0
+                    && dy >= 0
+                    && dx < N_GRID as i32
+                    && dy < N_GRID as i32
+                    && !visited[dx as usize][dy as usize]
+                {
+                    visited[dx as usize][dy as usize] = true;
+                    x_cords.push(dx as usize);
+                    y_cords.push(dy as usize);
+                }
             }
         }
+
+        let start = Instant::now();
+        let checker = GridChecker::new(&x_cords, &y_cords);
+        let alive = checker.check(&self.grid);
+        println!("Grid checker: {:?}", start.elapsed());
+
+
+        let mut visited = vec![vec![false; N_GRID]; N_GRID];
+        let mut next = Vec::with_capacity(alive.len());
+        for cell in alive {
+            if visited[cell.x][cell.y] {
+                continue;
+            }
+            visited[cell.x][cell.y] = true;
+            self.grid[cell.x][cell.y] = cell.val;
+            if self.grid[cell.x][cell.y] {
+                next.push((cell.x, cell.y));
+            }
+        }
+        self.alive = next;
     }
 
     pub fn toggle(&mut self, x: usize, y: usize) {
         self.grid[x][y] = !self.grid[x][y];
-    }
-}
-
-fn verify_cell(old: &[[bool;  N_GRID]; N_GRID], grid: &mut [[bool; N_GRID]; N_GRID], x: usize, y: usize) {
-    let mut number_of_living_neighbors = 0;
-    for offset in OFFSETS {
-        let n_x = sum(x, offset.x);
-        let n_y = sum(y, offset.y);
-
-        if n_x >= N_GRID as i32 || n_y >= N_GRID as i32 || n_x < 0 || n_y < 0 { 
-            continue;
-        }
-
-        if old[n_x as usize][n_y as usize] {
-            number_of_living_neighbors += 1;
-        }
-    }
-    if number_of_living_neighbors == 3 {
-        grid[x][y] = true;
-    } else if number_of_living_neighbors == 2 {
-    } else {
-        grid[x][y] = false;
+        self.alive.push((x, y));
     }
 }
 
 fn sum(a: usize, b: i32) -> i32 {
-    a as i32 + b 
+    a as i32 + b
+}
+
+#[derive(Debug)]
+struct SetCell {
+    x: usize,
+    y: usize,
+    val: bool,
+}
+
+struct GridChecker<'a> {
+    x: &'a [usize],
+    y: &'a [usize],
+}
+
+impl<'a> GridChecker<'a> {
+
+    fn new(x: &'a [usize], y: &'a [usize]) -> Self {
+        GridChecker { x, y }
+    }
+
+    pub fn check(&self, grid: &[[bool; N_GRID]; N_GRID]) -> Vec<SetCell> {
+        (0..self.x.len())
+            .map(|i| self.verify_cell(self.x[i], self.y[i], grid))
+            .collect()
+    }
+
+    fn verify_cell(&self, x: usize, y: usize, grid: &[[bool; N_GRID]; N_GRID]) -> SetCell {
+        let mut number_of_living_neighbors = 0;
+        for offset in OFFSETS {
+            let n_x = sum(x, offset.0);
+            let n_y = sum(y, offset.1);
+
+            if n_x >= N_GRID as i32 || n_y >= N_GRID as i32 || n_x < 0 || n_y < 0 {
+                continue;
+            }
+
+            if grid[n_x as usize][n_y as usize] {
+                number_of_living_neighbors += 1;
+            }
+        }
+        if number_of_living_neighbors == 3 {
+            SetCell { x, y, val: true }
+        } else if number_of_living_neighbors == 2 {
+            SetCell {
+                x,
+                y,
+                val: grid[x][y],
+            }
+        } else {
+            SetCell { x, y, val: false }
+        }
+    }
+
+
 }
